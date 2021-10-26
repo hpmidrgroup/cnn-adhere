1f45596a5c5141c6c231c262180dde7afd285a10
hpmidrgroup <51498672+hpmidrgroup@users.noreply.github.com>
2021-10-26 18:52:29 -0400

    Initial commit

---
+++ nncreate.py
@@ -0,0 +1,97 @@
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('Using {} device'.format(device))
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork().to(device)
+print(model)
+
+X = torch.rand(1, 28, 28, device=device)
+logits = model(X)
+pred_probab = nn.Softmax(dim=1)(logits)
+y_pred = pred_probab.argmax(1)
+print(f"Predicted class: {y_pred}")
+
+#layers = [
+ #   imageInputLayer([1944/2 2592/2 3])
+    
+  #  convolution2dLayer(3,8,'Padding','same')
+   # batchNormalizationLayer
+    #reluLayer
+    
+    #maxPooling2dLayer(2,'Stride',2)
+    
+    
+   # convolution2dLayer(3,16,'Padding','same')
+   # batchNormalizationLayer
+   # reluLayer
+    
+    #maxPooling2dLayer(2,'Stride',2)
+    
+    #convolution2dLayer(3,32,'Padding','same')
+    #batchNormalizationLayer
+    #reluLayer
+    
+    #fullyConnectedLayer(3)
+    #softmaxLayer
+    #classificationLayer];
+
+#Initialize flatten layer
+input_image = torch.rand(3,28,28)
+print(input_image.size())
+flatten = nn.Flatten()
+flat_image = flatten(input_image)
+print(flat_image.size())
+
+#Initialize linear layer
+#The linear layer is a module that applies a linear transformation on the input using its stored weights and biases.
+
+layer1 = nn.Linear(in_features=28*28, out_features=20)
+hidden1 = layer1(flat_image)
+print(hidden1.size())
+
+#Non-linear activation
+print(f"Before ReLU: {hidden1}\n\n")
+hidden1 = nn.ReLU()(hidden1)
+print(f"After ReLU: {hidden1}")
+
+#Put together ordered containers of modules
+seq_modules = nn.Sequential(
+flatten,
+layer1,
+nn.ReLU(),
+nn.Linear(20, 10)
+)
+input_image = torch.rand(3,28,28)
+logits = seq_modules(input_image)
+
+#The last linear layer of the neural network returns logits - raw values in [-infty, infty] - which are passed to the nn.Softmax module. The logits are scaled to values [0, 1] representing the model’s predicted probabilities for each class. dim parameter indicates the dimension along which the values must sum to 1.
+#Applies the Softmax function to an n-dimensional input Tensor rescaling them so that the elements of the n-dimensional output Tensor lie in the range [0,1] and sum to 1.
+softmax = nn.Softmax(dim=1)
+pred_probab = softmax(logits)
+
+print("Model structure: ", model, "\n\n")
+
+for name, param in model.named_parameters():
+    print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
